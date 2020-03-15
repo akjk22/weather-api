e25e62d931fee7f919e481c2446b161bd3bdf8de
Andrew Kim <andrewswkim47@gmail.com>
2020-03-15 15:03:47 -0700

    WIP promise branch for weather api.

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import $ from 'jquery';
+
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    $('#location').val("");
+
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[d43df70f2a4de3ad6bd547ffebf41550]`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+    }, function(error) {
+      $('.errors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
+});
